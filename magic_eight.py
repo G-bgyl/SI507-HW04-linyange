f785efe052fa88664d1013edfde0f97c698f9f7b
G-bgyl <gelinyan0801@foxmail.com>
2018-01-29 15:35:32 -0500

    first commit

--- magic_eight.py
+++ magic_eight.py
@@ -1,9 +1,12 @@
-import random
 def question():
-	answer=input("What is your question?")
-	ans_list = ["It is certain","It is decidedly so", "Without a doubt"]
-	strrr="● Yes definitely● You may rely on it● As I see it, yes● Most likely● Outlook good● Yes● Signs point to yes● Reply hazy try again● Ask again later● Better not tell you now● Cannot predict now● Concentrate and ask again● Don't count on it● My reply is no● My sources say no● Outlook not so good● Very doubtful"
-	ans_list=ans_list+strrr.split('●')
-	answer=random.choice(ans_list)
+    answer=input("What is your question?")
 
-question()
+# import random
+# def question():
+# 	answer=input("What is your question?")
+# 	ans_list = ["It is certain","It is decidedly so", "Without a doubt"]
+# 	strrr="● Yes definitely● You may rely on it● As I see it, yes● Most likely● Outlook good● Yes● Signs point to yes● Reply hazy try again● Ask again later● Better not tell you now● Cannot predict now● Concentrate and ask again● Don't count on it● My reply is no● My sources say no● Outlook not so good● Very doubtful"
+# 	ans_list=ans_list+strrr.split('●')
+# 	answer=random.choice(ans_list)
+#
+# question()
